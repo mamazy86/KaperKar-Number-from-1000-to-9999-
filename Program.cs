36bf4f194cc344095f8e440300511fae2ecfc6b3
Mohamadreza Panahandeh <mdrezapanahandeh@gmail.com>
2026-02-03 20:37:27 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
 // Mohamadreza Panahandeh
 
 
-for (int i = 1000; i < 9999; i++)
+for (int i = 1000; i <= 9999; i++)
 {
     Console.WriteLine($"\vresult of Kaperkar number for {i} :");
     CalculateIterations calculate = new CalculateIterations();
     string result = calculate.KaperkarNumber(i);
     Console.WriteLine($"\v{result}");
+
 }
